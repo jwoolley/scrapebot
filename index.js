7476fdbaeb712274cc50be0210342b9d8788a6e4
Justin Woolley <19597672+jwoolley@users.noreply.github.com>
2018-09-17 01:58:08 -0700

    working login

--- index.js
+++ index.js
@@ -11,10 +11,17 @@ const globals = {
 
 const LOCATORS = {
 	LOGIN: {
-		LINK: By.css('a[title="Login"]'),
-		BOGUS: By.css('a[title="Bogus"]')
+		LINK: By.css('#nav-main a[title="Login"]'),
+		FORM: {
+			CONTAINER: By.css('div.login_form'),
+			FIELD: {
+				USERNAME: By.id('username'),
+				PASSWORD: By.id('password')
+			},
+			SUBMIT: By.css('div.login_form input[name="login"]')
+		}	
 	}
-}
+};
 
 const log = Object.assign({}, console, { debug: function() { globals.DEBUG && console.debug.call(this, ...arguments); } });
 
@@ -33,8 +40,10 @@ async function waitForElement(locator, timeoutInMs=5000) {
 	return new Promise(async (resolve, reject) => {
 		try {
 			await globals.browser.wait(until.elementLocated(locator), timeoutInMs);
-			log.debug('Located element.');
-			resolve();
+			const element = await globals.browser.findElement(locator);		
+			log.debug('Located element. Waiting for it to be visible...');			
+			await globals.browser.wait(until.elementIsVisible(element), timeoutInMs);
+			resolve(element);
 		} catch (e) {
 			reject(e);
 		}
@@ -42,34 +51,53 @@ async function waitForElement(locator, timeoutInMs=5000) {
 }
 
 const waitForUrl = async function(expectedUrl, timeout=5000, interval=500) {
-	const promise = new Promise(async (resolve, reject) => {
+	return new Promise(async (resolve, reject) => {
+		setTimeout(reject, timeout);
 
-		let timedOut = false;
-		let url = await browser.getCurrentUrl();
-		console.debug('Current url: ', url);
-
-		while (!timedOut) {
-			if (url === expectedUrl) {
-				resolve();
-				return;
-			}
+		let url = await globals.browser.getCurrentUrl();
+		while (url !== expectedUrl) {
+			log.debug(`Url not found. Waiting for ${interval}ms to retry...`, expectedUrl);
+			await sleep(interval);
+			url = globals.browser.getCurrentUrl();
 		}
+		resolve();
 	});
+}
 
-	return promise;
+async function navigateWithLink(linkElement) {
+	const linkUrl = await linkElement.getAttribute('href');
+	await linkElement.click();
+	await waitForUrl(linkUrl);
+}
+
+async function login(username, password) {
+	const browser = globals.browser;
+	await browser.get(forumRootUrl);
+
+	try {
+		const loginLink = await waitForElement(LOCATORS.LOGIN.LINK, 10000);
+		await navigateWithLink(loginLink);
+
+		log.debug('Waiting for login form to be visible...');
+		await waitForElement(LOCATORS.LOGIN.FORM.CONTAINER);
+
+		const usernameField = await waitForElement(LOCATORS.LOGIN.FORM.FIELD.USERNAME);
+		const passwordField = await waitForElement(LOCATORS.LOGIN.FORM.FIELD.PASSWORD);
+
+		await usernameField.sendKeys(username);
+		await passwordField.sendKeys(password);
+
+		const loginButton = await waitForElement(LOCATORS.LOGIN.FORM.SUBMIT);
+		await loginButton.click();
+		log.debug('done!')
+	} catch (e) {
+		log.error('Error:', e);
+	}
 }
 
 async function run() {
 	log.debug('starting...')	
 	globals.browser = getBrowser(globals.webdriver);
-
-	const browser = globals.browser;
-	await browser.get(forumRootUrl);
-	try {
-		await waitForElement(LOCATORS.LOGIN.LINK);
-		log.debug('done.')
-	} catch (e) {
-		log.error('Error:', e);
-	}
+	await login('frenchy', 'booboo');
 }
 run();
